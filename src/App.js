3ac91472ac5ab8d0033da99ce0508372e412ff23
sergedontsa <dontsaserge@gmail.com>
2020-07-28 22:54:43 -0400

    drawer added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ApartmentIcon from '@material-ui/icons/Apartment';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import GavelIcon from '@material-ui/icons/Gavel';
+import GroupWorkIcon from '@material-ui/icons/GroupWork';
+import HearingIcon from '@material-ui/icons/Hearing';
+import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import SettingsIcon from '@material-ui/icons/Settings';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
 
 const drawerWidth = 240;
 
@@ -24,62 +33,69 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: 36,
   },
   hide: {
     display: 'none',
   },
   drawer: {
+    backgroundColor:"red",
     width: drawerWidth,
     flexShrink: 0,
+    whiteSpace: 'nowrap',
   },
-  drawerPaper: {
+  drawerOpen: {
     width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
-  drawerHeader: {
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'flex-end',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
   },
 }));
 
-export default function App() {
+export default function MiniDrawer() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const styles = {paper:{background:'red'}}
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -104,69 +120,101 @@ export default function App() {
                 aria-label="open drawer"
                 onClick={handleDrawerOpen}
                 edge="start"
-                className={clsx(classes.menuButton, open && classes.hide)}
+                className={clsx(classes.menuButton, {
+                  [classes.hide]: open,
+                })}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h0" noWrap>
-              Persistent drawer
+            <Typography variant="h6" noWrap>
+              Housing Authority
             </Typography>
           </Toolbar>
         </AppBar>
-        <Drawer className={classes.drawer} variant="persistent" anchor="left" open={open} classes={{paper: classes.drawerPaper,}}>
-          <div className={classes.drawerHeader}>
+        <Drawer
+            variant="permanent"
+            className={clsx(classes.drawer, {
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            })}
+            classes={{
+              paper: clsx({
+                [classes.drawerOpen]: open,
+                [classes.drawerClose]: !open,
+
+
+              }),
+
+
+
+
+            }}
+        >
+          <div className={classes.toolbar}>
             <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ArrowBackIosIcon/>: <ArrowBackIosIcon/>}
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
             </IconButton>
           </div>
           <Divider />
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
+            <ListItem button>
+              <ListItemIcon><LocationCityIcon/></ListItemIcon>
+              <ListItemText primary={(`Building`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><ApartmentIcon/></ListItemIcon>
+              <ListItemText primary={(`Apartment`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><ReceiptIcon/></ListItemIcon>
+              <ListItemText primary={(`Billing`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><GavelIcon/></ListItemIcon>
+              <ListItemText primary={(`Complain`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><GroupWorkIcon/></ListItemIcon>
+              <ListItemText primary={(`Employee`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><HearingIcon/></ListItemIcon>
+              <ListItemText primary={(`Listening`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><PersonPinCircleIcon/></ListItemIcon>
+              <ListItemText primary={(`Tenant`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><ScheduleIcon/></ListItemIcon>
+              <ListItemText primary={(`Schedule`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><SubscriptionsIcon/></ListItemIcon>
+              <ListItemText primary={(`Subscriber`).toUpperCase()}/>
+            </ListItem>
+            <Divider/>
+            <ListItem button>
+              <ListItemIcon><SettingsIcon/></ListItemIcon>
+              <ListItemText primary={(`Setting`).toUpperCase()}/>
+            </ListItem>
+
           </List>
           <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
-          </List>
+
         </Drawer>
-        <main
-            className={clsx(classes.content, {
-              [classes.contentShift]: open,
-            })}
-        >
-          <div className={classes.drawerHeader} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-          </Typography>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+
+
         </main>
       </div>
   );
